e8b558e292c30ce93a6a166c923321154dcfa99e
Vladyslav Makarenko <makarenko.vlad.112@gmail.com>
2024-02-26 19:04:42 +0100

    Production 1.0 +50min
    Added loading event

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import iziToast from 'izitoast';
 import { fetchImages } from '/js/pixabay-api.js';
 import { renderImages } from '/js/render-functions.js';
+import errorIcon from '/img/error-icon.png';
 
 const FORM = document.querySelector('.form');
 const INPUT = document.querySelector('.search-input');
@@ -10,7 +11,7 @@ const GALLERY = document.querySelector('.gallery');
 
 const SIMPLY_LIGHTBOX = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
-  captionDelay: 250,
+  captionDelay: 150,
   captionsPosition: 'bottom',
 });
 
@@ -19,9 +20,9 @@ FORM.addEventListener('submit', searchPhotos);
 function searchPhotos(event) {
   event.preventDefault();
 
-  const query = INPUT.value;
+  GALLERY.innerHTML = `<span class="loader"></span>`;
 
-  fetchImages(query)
+  fetchImages(INPUT.value)
     .then(data => {
       const images = data.hits;
 
@@ -30,10 +31,11 @@ function searchPhotos(event) {
       }
 
       renderImages(images, GALLERY);
-
       SIMPLY_LIGHTBOX.refresh();
     })
     .catch(() => {
+      GALLERY.innerHTML = "";
+      
       iziToast.error({
         message:
           'Sorry, there are no images matching your search query. Please try again!',
@@ -41,7 +43,9 @@ function searchPhotos(event) {
         timeout: 5000,
         progressBar: true,
         close: true,
-        messageColor: '#000',
+        iconUrl: errorIcon,
+        messageColor: '#fafafb',
+        iconColor: '#fafafb',
         backgroundColor: '#FF544B',
       });
     })
